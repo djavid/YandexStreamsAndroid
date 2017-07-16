aa5002ebb972eb56b241c2d10ed814b3117351ea
Tetawex <tetawex@gmail.com.com>
2017-07-16 16:10:43 +0300

    Base architecture

--- app/src/main/java/net/azurewebsites/streambeta/yandexstreamsandroid/domain/presenter/interfaces/MainPresenter.java
+++ app/src/main/java/net/azurewebsites/streambeta/yandexstreamsandroid/domain/presenter/interfaces/MainPresenter.java
@@ -1,11 +1,15 @@
 package net.azurewebsites.streambeta.yandexstreamsandroid.domain.presenter.interfaces;
 
+import net.azurewebsites.streambeta.yandexstreamsandroid.core.Presenter;
+import net.azurewebsites.streambeta.yandexstreamsandroid.domain.presenter.instancestate.MainPresenterInstanceState;
+import net.azurewebsites.streambeta.yandexstreamsandroid.domain.router.MainRouter;
 import net.azurewebsites.streambeta.yandexstreamsandroid.domain.router.ScreenTag;
+import net.azurewebsites.streambeta.yandexstreamsandroid.domain.view.interfaces.MainView;
 
 /**
  * Created by Tetawex on 15.07.2017.
  */
 
-public interface MainPresenter {
+public interface MainPresenter extends Presenter<MainView, MainRouter, MainPresenterInstanceState>{
     void onScreenSwitched(ScreenTag screenTag);
 }
